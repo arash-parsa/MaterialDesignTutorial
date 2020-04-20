f98e7cbb3fb37789d1498caf3d89599f4ebde1de
Arash <arashparsa.work@gmail.com>
2020-04-20 23:46:49 +0430

    Initial commit

---
+++ app/src/main/java/com/example/materialdesigntutorial/FragmentMain.java
@@ -0,0 +1,67 @@
+package com.example.materialdesigntutorial;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.card.MaterialCardView;
+
+public class FragmentMain extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar_main);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+
+
+
+
+        final MaterialCardView materialCardView = view.findViewById(R.id.card_main_text);
+        materialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.add(R.id.frame_main_fragmentContainer, new DetailFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+    }
+}
+
+
+
+
+     /*   materialCardView.setChecked(true);
+        materialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialCardView.setChecked(!materialCardView.isChecked());
+            }
+        });
+
+
+
+
+    }*/
