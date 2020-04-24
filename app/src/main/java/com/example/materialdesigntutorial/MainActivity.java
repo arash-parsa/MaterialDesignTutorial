eb0c69aee367d73ebd0fa87a3323a8cffd48e132
Arash <arashparsa.work@gmail.com>
2020-04-24 10:04:08 +0430

    Bottom Navigation

--- app/src/main/java/com/example/materialdesigntutorial/MainActivity.java
+++ app/src/main/java/com/example/materialdesigntutorial/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.materialdesigntutorial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +18,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.frame_main_fragmentContainer,new FragmentMain());
-        fragmentTransaction.commit();
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation_main);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menu_home:
+                    FragmentTransaction fragmentTransaction1= getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction1.replace(R.id.frame_main_fragmentContainer,new FragmentMain());
+                    fragmentTransaction1.commit();
+                    break;
+                    case R.id.menu_nearby:
+                        FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction2.replace(R.id.frame_main_fragmentContainer,new NearbyFragment());
+                        fragmentTransaction2.commit();
+                        break;
+                    case R.id.menu_recents:
+                        FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction3.replace(R.id.frame_main_fragmentContainer,new RecentsFragment());
+                        fragmentTransaction3.commit();
+                        break;
+                }
+
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+            }
+        });
 
     }
 }
